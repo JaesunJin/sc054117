f231bd45eebe42a14fb2d8b7c72b783fa851e849
JaesunJin <60768423+JaesunJin@users.noreply.github.com>
2020-03-01 20:45:23 +0900

    Add files via upload

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func lenAndUpper(name string) (lenght int, uppercase string) {
+	defer fmt.Println("i'm done")
 	lenght = len(name)
 	uppercase = strings.ToUpper(name)
 	return
@@ -15,8 +16,47 @@ func repeatMe(words ...string) {
 	fmt.Println(words)
 }
 
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
+
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	}
+	return true
+}
+
+func arr() {
+	names := []string{"j", "a"}
+	names = append(names, "s")
+	fmt.Println(names)
+}
+
+func learnMap() {
+	jason := map[string]string{"name": "jason", "age": "30"}
+	for key, value := range jason {
+		fmt.Println(key, value)
+	}
+}
+
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
+func learnStructure() {
+	favFood := []string{"gyoza", "potato"}
+	jason := person{name: "jason", age: 32, favFood: favFood}
+	fmt.Println(jason)
+}
+
 func main() {
-	totalLenght, uppercase := lenAndUpper("jason")
-	fmt.Println(totalLenght, uppercase)
-	repeatMe("jason", "jin")
+	// learnMap()
+	learnStructure()
 }
